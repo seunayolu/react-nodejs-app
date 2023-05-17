9d1b8cca59622b1b3201f172b0d8b66a67f00530
seunayolu <seunayolu@gmail.com>
2023-05-17 16:15:46 +0100

    groovy script modified

--- script.groovy
+++ script.groovy
@@ -1,26 +1,27 @@
+//Build the Docker Image Using the Dockerfile in the root folder
+
 def buildImage() {
     echo "Build Docker Image with Dockerfile..."
-    //withCredentials([usernamePassword(credentialsId: 'Docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
     sh 'docker build -t oluwaseuna/demo-app:react-nodejs-app .'
-        //sh "echo $PASS | docker login -u $USER --password-stdin"
-        //sh 'docker push oluwaseuna/demo-app:react-nodejs-app'
 }
 
+//Push Docker Image to DockerHub Repository
 
 def pushImage() {
     echo "Pushing Docker Image to Docker Hub Repo..."
     withCredentials([usernamePassword(credentialsId: 'Docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        //sh 'docker build -t oluwaseuna/demo-app:react-nodejs-app .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push oluwaseuna/demo-app:react-nodejs-app'
     }
 } 
+
+//Pull Docker Image from DockerHub and Deploy Image to Amazon EC2 Server
+
 def deployImage() {
-    echo "Deploy Docker Image to EC2..."
+    echo "Deploying the application to EC2..."
     def dockerCmd = 'docker run -d -p 80:80 --name react-nodejs-app oluwaseuna/demo-app:react-nodejs-app' 
     sshagent(['ec2-pem-key']) {
         sh "ssh -o StrictHostKeyChecking=no ec2-user@52.56.46.176 ${dockerCmd}"
-
     }
 } 
 return this
